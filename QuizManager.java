485ea728d19f7975493586208bd9c9338ca0780f
Nayeem Ahammed <nayeem.ahammed01@gmail.com>
2025-05-03 11:48:49 +0600

    update design

--- QuizManager.java
+++ QuizManager.java
@@ -18,7 +18,7 @@ public class QuizManager {
             this.quizzes = database.loadQuizzes();
 
         } catch (Exception e) {
-            Design.printDesign (Design.PURPLE, Main.tab, "Error loading quizzes: " , e.getMessage(), Design.RESET);
+            Design.printDesign (Design.PURPLE, Main.tab(), "Error loading quizzes: " , e.getMessage(), Design.RESET);
         }
     }
 
@@ -28,14 +28,15 @@ public class QuizManager {
             quizzes.add(quiz);
         try {
             database.saveQuiz(quiz);
-            Design.printDesign (Design.GREEN, Main.tab, "Quiz saved successfully!\n\n", Design.RESET);
+            Design.printDesign (Design.GREEN, Main.tab(), "Quiz saved successfully!\n\n", Design.RESET);
         } catch (Exception e) {
-            Design.printDesign (Design.PURPLE, Main.tab, "Error saving quiz: " , e.getMessage(), Design.RESET);
+            Design.printDesign (Design.PURPLE, Main.tab(), "Error saving quiz: " , e.getMessage(), Design.RESET);
         }
     }
 
     // Take a quiz
     public int takeQuiz (String quizId, Scanner input) {
+        Design.clearScreen();
         // if want to add time limit , add a timer here
             
         // find the quiz by id using java Streams
@@ -45,31 +46,32 @@ public class QuizManager {
                 .orElse(null);
 
         if (quiz == null) {
-            Design.printDesign (Design.RED, Main.tab, "Quiz not found!", Design.RESET);
+            Design.printDesign (Design.ITALIC, Design.PURPLE, Main.down(), Main.tab(), "Quiz not found!", Design.RESET);
             return -1;
         }
 
-        Design.printDesign(Design.CYAN, Main.tab, "Taking quiz: ", quiz.getTitle(), Design.RESET);
+        Design.printDesign("\n", Main.tab(), Design.UNDERLINE, Design.BOLD, Design.CYAN, "Taking quiz: ", quiz.getTitle(), Design.RESET, "\n\n");
         int score = 0;
         ArrayList<Question> questions = quiz.getQuestions();
 
         int n = 1;
-        for (Question question : quiz.getQuestions()) {
-            Design.printDesign(Main.tab, Design.RED, "Q", Integer.toString(n++) , " : ", Design.GREEN, question.getQuestionText(), Design.RESET);
+        for (Question question : questions) {
+            Design.printDesign(" ".repeat(Design.width()/4), Design.BOLD, Design.RED, "Q", Integer.toString(n++) , " : ", Design.GREEN, question.getQuestionText(), Design.RESET, "\n");
 
             ArrayList<String> options = question.getOptions();
             for(int i = 0; i < options.size(); i++) {
-                Design.printDesign (Main.tab, Design.YELLOW, "  ", Integer.toString(i + 1), ". ", Design.CYAN, options.get(i), Design.RESET);
+                Design.printDesign (" ".repeat(Design.width()/4), Design.BLUE, Design.BOLD, "  ", Integer.toString(i + 1), ". ", Design.WHITE, options.get(i), Design.RESET + "\n");
             }
 
-            Design.printDesign (Main.tab, Design.YELLOW, "Enter your answer (1-" + options.size() + "): ", Design.RESET);
+            Design.printDesign (" ".repeat(Design.width()/4), Design.YELLOW, "Enter your answer (1-" + options.size() + "): ", Design.RESET);
             int answer = input.nextInt();
             
             if(answer < 1 || answer > options.size()) {
                 // invalid answer
             }
 
-            if(question.getCorrectAnswerIndex() == answer - 1) score++;
+            if(question.getCorrectAnswerIndex() == answer) score++;
+            // System.out.println("\n\n");
         }
         return score;
     }
